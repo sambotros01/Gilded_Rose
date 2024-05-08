43ff17de390ea94e926aaa2669d8e48c4b97ae56
Sam <sambotros01@gmail.com>
2024-05-08 12:02:33 -0600

    Testing Complete

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -11,47 +11,49 @@ class Shop {
     this.items = items;
   }
   updateQuality() {
+
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
+            this.items[i].quality = this.items[i].quality - 1; //Item quality decreases by one for normal items (Done)
           }
         }
+
       } else {
         if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
+          this.items[i].quality = this.items[i].quality + 1; // Any not normal item, increase by 1 (Done)
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.items[i].quality = this.items[i].quality + 1; // Backstage Ticket +2 (Done)
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.items[i].quality = this.items[i].quality + 1; // Backstage Ticket +3 (Done)
               }
             }
           }
         }
       }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        this.items[i].sellIn = this.items[i].sellIn - 1; // Decrease all items days to sell by one (except Sulfuras) (Done)
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
+                this.items[i].quality = this.items[i].quality - 1; // Decrease twice as fast after Sell In Day (Done)
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            this.items[i].quality = this.items[i].quality - this.items[i].quality; // Concert quality is 0 after concert date (Done)
           }
         } else {
           if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+            this.items[i].quality = this.items[i].quality + 1; // cheese - if below max quality, increase
           }
         }
       }
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -10,13 +10,26 @@ describe("Gilded Rose Pin Down Tests", () => {
     expect(items[0].quality).toBe(19); //check
   });
 
-  test('Quality of "Aged Brie" should increase by 1 each day', () => {
+  test('Quality of "Aged Brie" should increase by 1 each day only if quality is below 50', () => {
     let agedBrie = new Item("Aged Brie", 10, 20);
-    const gildedRose = new Shop([agedBrie]);
+    let maxAgedBrie = new Item("Aged Brie", 10, 50);
+    const gildedRose = new Shop([agedBrie, maxAgedBrie]);
 
     const items = gildedRose.updateQuality();
 
     expect(items[0].quality).toBe(21);
+    expect(items[1].quality).toBe(50);
+  });
+
+  test('Quality of "Backstage passes" should increase by 2 when there are 10 days - 5 days till concert', () => {
+    let backstagePass = new Item(
+      "Backstage passes to a TAFKAL80ETC concert",
+      10,
+      20
+    );
+    const gildedRose = new Shop([backstagePass]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(22);
   });
 
   test('Quality of "Backstage passes" should increase by 3 when there are 5 days or less', () => {
@@ -28,5 +41,34 @@ describe("Gilded Rose Pin Down Tests", () => {
     const gildedRose = new Shop([backstagePass]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(23);
+  });
+
+  test('Normal items should degrade in quality by 2 after sell by date has passed', () => {
+    let normalItem = new Item("normal", 0, 20);
+    const gildedRose = new Shop([normalItem]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(18)
+  })
+
+  test('SellIn date reduces by one each day except for Sulfuras', () => {
+    let normalItem = new Item("normal", 10, 20); //build
+    let sulfurasItem = new Item('Sulfuras, Hand of Ragnaros', 10, 20);
+    const gildedRose = new Shop([normalItem, sulfurasItem]);
+
+    const items = gildedRose.updateQuality(); //operate
+
+    expect(items[0].sellIn).toBe(9); //check
+    expect(items[1].sellIn).toBe(10); //check
+  })
+
+  test('Quality of "Backstage passes" should be 0 if sellIn is 0', () => {
+    let backstagePass = new Item(
+      "Backstage passes to a TAFKAL80ETC concert",
+      0,
+      20
+    );
+    const gildedRose = new Shop([backstagePass]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
   });
 });
