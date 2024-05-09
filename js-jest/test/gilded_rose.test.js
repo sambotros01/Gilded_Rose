424ed91b1244b6c55470b946af45c1c739d3cb51
Sam <sambotros01@gmail.com>
2024-05-09 13:48:33 -0600

    Styling Changes

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -3,12 +3,14 @@ const { Item, Shop } = require("../src/gilded_rose.js");
 describe("Gilded Rose Pin Down Tests", () => {
   test ('Shop initializes items as an empty array', () => {
     let gildedRose = new Shop()
+
     expect (gildedRose.items.length).toBe(0)
   })
 
   test('Each item should contain a name, sell by date and quality value', () => {
     let normalItem = new Item("normal", 15, 45);
     const gildedRose = new Shop([normalItem]);
+
     const item = gildedRose.items[0]
 
     expect(item.name).toEqual("normal");
@@ -16,13 +18,14 @@ describe("Gilded Rose Pin Down Tests", () => {
     expect(item.quality).toBe(45);
 
   })
+
   test("Normal items should degrade in quality by 1 each day", () => {
-    let normalItem = new Item("normal", 10, 20); //build
+    let normalItem = new Item("normal", 10, 20);
     const gildedRose = new Shop([normalItem]);
 
-    const items = gildedRose.updateQuality(); //operate
+    const items = gildedRose.updateQuality();
 
-    expect(items[0].quality).toBe(19); //check
+    expect(items[0].quality).toBe(19);
   });
 
   test('Quality of "Aged Brie" should increase by 1 each day only if quality is below 50', () => {
@@ -37,18 +40,20 @@ describe("Gilded Rose Pin Down Tests", () => {
   });
 
   test('Quality of "Backstage passes" should increase by 2 when there are 10 days - 5 days till concert', () => {
-    let backstagePass = new Item(
-      "Backstage passes to a TAFKAL80ETC concert", 9, 20);
+    let backstagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 9, 20);
     const gildedRose = new Shop([backstagePass]);
+
     const items = gildedRose.updateQuality();
+
     expect(items[0].quality).toBe(22);
   });
 
   test('Quality of "Backstage passes" should increase by 3 when there are 5 days or less', () => {
-    let backstagePass = new Item(
-      "Backstage passes to a TAFKAL80ETC concert", 4, 20);
+    let backstagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 4, 20);
     const gildedRose = new Shop([backstagePass]);
+
     const items = gildedRose.updateQuality();
+
     expect(items[0].quality).toBe(23);
   });
 
@@ -56,30 +61,30 @@ describe("Gilded Rose Pin Down Tests", () => {
     let normalItem = new Item("normal", -1, 20);
     let edgeNormalItem = new Item("normal", -1, 1);
     const gildedRose = new Shop([normalItem, edgeNormalItem]);
+
     const items = gildedRose.updateQuality();
+
     expect(items[0].quality).toBe(18)
     expect(items[1].quality).toBe(0)
   })
 
   test('SellIn date reduces by one each day except for Sulfuras', () => {
-    let normalItem = new Item("normal", 10, 20); //build
+    let normalItem = new Item("normal", 10, 20);
     let sulfurasItem = new Item('Sulfuras, Hand of Ragnaros', 10, 80);
     const gildedRose = new Shop([normalItem, sulfurasItem]);
 
-    const items = gildedRose.updateQuality(); //operate
+    const items = gildedRose.updateQuality();
 
-    expect(items[0].sellIn).toBe(9); //check
-    expect(items[1].sellIn).toBe(10); //check
+    expect(items[0].sellIn).toBe(9);
+    expect(items[1].sellIn).toBe(10);
   })
 
   test('Quality of "Backstage passes" should be 0 if sellIn is less than 0', () => {
-    let backstagePass = new Item(
-      "Backstage passes to a TAFKAL80ETC concert",
-      -1,
-      20
-    );
+    let backstagePass = new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20);
     const gildedRose = new Shop([backstagePass]);
+
     const items = gildedRose.updateQuality();
+
     expect(items[0].quality).toBe(0);
   });
 
@@ -99,7 +104,9 @@ describe("Gilded Rose Pin Down Tests", () => {
   test('Sulfuras items have a default quality value of 80', () => {
     let sulfurasItem = new Item("sulfuras", 10, 70)
     const gildedRose = new Shop([sulfurasItem]);
+
     const items = gildedRose.updateQuality();
+
     expect(items[0].quality).toBe(80);
   })
 });
